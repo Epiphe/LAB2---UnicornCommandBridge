36069fbd958d685bfef55566da84d9d6d6b5c525
Epiphe <epiphe@gmail.com>
2019-12-30 12:16:08 +0100

    Added buttons

--- LAB2 - UnicornCommandBridge/Form1.Designer.cs
+++ LAB2 - UnicornCommandBridge/Form1.Designer.cs
@@ -28,13 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSkapaStudent = new System.Windows.Forms.Button();
+            this.btnSkapaLarare = new System.Windows.Forms.Button();
+            this.btnSkapaKurs = new System.Windows.Forms.Button();
+            this.btnSkapaUppgift = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSattaBetyg = new System.Windows.Forms.Button();
+            this.btnRedigeraBetyg = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnRedigeraUppgift = new System.Windows.Forms.Button();
+            this.btnRedigeraKurs = new System.Windows.Forms.Button();
+            this.btnRedigeraLarare = new System.Windows.Forms.Button();
+            this.btnRedigeraStudent = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Välkommen till din Unicorn command bridge.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSkapaUppgift);
+            this.groupBox1.Controls.Add(this.btnSkapaKurs);
+            this.groupBox1.Controls.Add(this.btnSkapaLarare);
+            this.groupBox1.Controls.Add(this.btnSkapaStudent);
+            this.groupBox1.Location = new System.Drawing.Point(27, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 144);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lägg till/skapa:";
+            // 
+            // btnSkapaStudent
+            // 
+            this.btnSkapaStudent.Location = new System.Drawing.Point(7, 20);
+            this.btnSkapaStudent.Name = "btnSkapaStudent";
+            this.btnSkapaStudent.Size = new System.Drawing.Size(187, 23);
+            this.btnSkapaStudent.TabIndex = 0;
+            this.btnSkapaStudent.Text = "Student";
+            this.btnSkapaStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnSkapaLarare
+            // 
+            this.btnSkapaLarare.Location = new System.Drawing.Point(7, 49);
+            this.btnSkapaLarare.Name = "btnSkapaLarare";
+            this.btnSkapaLarare.Size = new System.Drawing.Size(187, 23);
+            this.btnSkapaLarare.TabIndex = 1;
+            this.btnSkapaLarare.Text = "Lärare";
+            this.btnSkapaLarare.UseVisualStyleBackColor = true;
+            // 
+            // btnSkapaKurs
+            // 
+            this.btnSkapaKurs.Location = new System.Drawing.Point(7, 78);
+            this.btnSkapaKurs.Name = "btnSkapaKurs";
+            this.btnSkapaKurs.Size = new System.Drawing.Size(187, 23);
+            this.btnSkapaKurs.TabIndex = 2;
+            this.btnSkapaKurs.Text = "Kurs";
+            this.btnSkapaKurs.UseVisualStyleBackColor = true;
+            // 
+            // btnSkapaUppgift
+            // 
+            this.btnSkapaUppgift.Location = new System.Drawing.Point(6, 107);
+            this.btnSkapaUppgift.Name = "btnSkapaUppgift";
+            this.btnSkapaUppgift.Size = new System.Drawing.Size(187, 23);
+            this.btnSkapaUppgift.TabIndex = 3;
+            this.btnSkapaUppgift.Text = "Uppgift";
+            this.btnSkapaUppgift.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRedigeraBetyg);
+            this.groupBox2.Controls.Add(this.btnSattaBetyg);
+            this.groupBox2.Location = new System.Drawing.Point(27, 250);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 83);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Betyg";
+            // 
+            // btnSattaBetyg
+            // 
+            this.btnSattaBetyg.Location = new System.Drawing.Point(7, 20);
+            this.btnSattaBetyg.Name = "btnSattaBetyg";
+            this.btnSattaBetyg.Size = new System.Drawing.Size(186, 23);
+            this.btnSattaBetyg.TabIndex = 0;
+            this.btnSattaBetyg.Text = "Sätta betyg";
+            this.btnSattaBetyg.UseVisualStyleBackColor = true;
+            // 
+            // btnRedigeraBetyg
+            // 
+            this.btnRedigeraBetyg.Location = new System.Drawing.Point(6, 49);
+            this.btnRedigeraBetyg.Name = "btnRedigeraBetyg";
+            this.btnRedigeraBetyg.Size = new System.Drawing.Size(186, 23);
+            this.btnRedigeraBetyg.TabIndex = 1;
+            this.btnRedigeraBetyg.Text = "Redigera betyg";
+            this.btnRedigeraBetyg.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnRedigeraUppgift);
+            this.groupBox3.Controls.Add(this.btnRedigeraKurs);
+            this.groupBox3.Controls.Add(this.btnRedigeraLarare);
+            this.groupBox3.Controls.Add(this.btnRedigeraStudent);
+            this.groupBox3.Location = new System.Drawing.Point(276, 70);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 144);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Redigera:";
+            // 
+            // btnRedigeraUppgift
+            // 
+            this.btnRedigeraUppgift.Location = new System.Drawing.Point(6, 107);
+            this.btnRedigeraUppgift.Name = "btnRedigeraUppgift";
+            this.btnRedigeraUppgift.Size = new System.Drawing.Size(187, 23);
+            this.btnRedigeraUppgift.TabIndex = 3;
+            this.btnRedigeraUppgift.Text = "Uppgift";
+            this.btnRedigeraUppgift.UseVisualStyleBackColor = true;
+            // 
+            // btnRedigeraKurs
+            // 
+            this.btnRedigeraKurs.Location = new System.Drawing.Point(7, 78);
+            this.btnRedigeraKurs.Name = "btnRedigeraKurs";
+            this.btnRedigeraKurs.Size = new System.Drawing.Size(187, 23);
+            this.btnRedigeraKurs.TabIndex = 2;
+            this.btnRedigeraKurs.Text = "Kurs";
+            this.btnRedigeraKurs.UseVisualStyleBackColor = true;
+            // 
+            // btnRedigeraLarare
+            // 
+            this.btnRedigeraLarare.Location = new System.Drawing.Point(7, 49);
+            this.btnRedigeraLarare.Name = "btnRedigeraLarare";
+            this.btnRedigeraLarare.Size = new System.Drawing.Size(187, 23);
+            this.btnRedigeraLarare.TabIndex = 1;
+            this.btnRedigeraLarare.Text = "Lärare";
+            this.btnRedigeraLarare.UseVisualStyleBackColor = true;
+            // 
+            // btnRedigeraStudent
+            // 
+            this.btnRedigeraStudent.Location = new System.Drawing.Point(7, 20);
+            this.btnRedigeraStudent.Name = "btnRedigeraStudent";
+            this.btnRedigeraStudent.Size = new System.Drawing.Size(187, 23);
+            this.btnRedigeraStudent.TabIndex = 0;
+            this.btnRedigeraStudent.Text = "Student";
+            this.btnRedigeraStudent.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(523, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Unicorn command bridge";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSkapaUppgift;
+        private System.Windows.Forms.Button btnSkapaKurs;
+        private System.Windows.Forms.Button btnSkapaLarare;
+        private System.Windows.Forms.Button btnSkapaStudent;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnRedigeraBetyg;
+        private System.Windows.Forms.Button btnSattaBetyg;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnRedigeraUppgift;
+        private System.Windows.Forms.Button btnRedigeraKurs;
+        private System.Windows.Forms.Button btnRedigeraLarare;
+        private System.Windows.Forms.Button btnRedigeraStudent;
     }
 }
 
