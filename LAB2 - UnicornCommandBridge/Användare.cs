6cb6fdfda37b862a748dc5358f4083047ce5e2b0
Skalleper95 <55976308+Skalleper95@users.noreply.github.com>
2020-01-13 14:31:21 +0100

    Användare gjord, class

---
+++ LAB2 - UnicornCommandBridge/Användare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2___UnicornCommandBridge
+{
+    class Användare
+    {
+        public string användarId;
+        public string namn;
+
+        public Användare(string användarId, string namn)
+        {
+            this.användarId = användarId;
+            this.namn = namn;
+        }
+    }
+}
