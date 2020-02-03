1244376d8323b9c8da61758c37bcd61c12586a6a
luffare97 <daniel.agren@hotmail.se>
2020-02-03 21:46:31 +0100

    Datastore med interface

--- LAB2 - UnicornCommandBridge/Datastore.cs
+++ LAB2 - UnicornCommandBridge/Datastore.cs
@@ -8,62 +8,49 @@ namespace LAB2___UnicornCommandBridge
 {
 
 
-    public class Datastore
+    public class Datastore : IDatastore
     {
-
-        public List<Student> students = new List<Student>()
-        {
-            new Student("S100", "Anders"),
-            new Student("S101", "Börje"),
-            new Student("S102", "Uffe"),
-            new Student("S103", "Koffe"),
-            new Student("S104", "Fredrik"),
-            new Student("S105", "Johanes"),
-            new Student("S106", "Louise")
-        };
-
-        public List<Betyg> sb = new List<Betyg>();
-
-        public Teacher Roger = new Teacher("T100", "Roger");
-        public Teacher Lisa = new Teacher("T101", "Lisa");
-
-
-        public List<Teacher> teachers = new List<Teacher>()
-        {
-           
-            new Teacher("T102", "Erik"),
-            new Teacher("T103", "Pelle"),
-            new Teacher("T104", "Johan"),
-            new Teacher("T105", "Magnus"),
-            new Teacher("T106", "Moa"),
-            new Teacher("T107", "Lena")
-        };
-
-        public Kurs Sys = new Kurs("SysAdm", "System administation");
-        public Kurs Web = new Kurs("WebDes", "WebbDesign");
-
-        public List<Kurs> kurser = new List<Kurs>()
-        {
-            
-        };
-        
-        public List<Betyg> ub = new List<Betyg>();
-
-        public Uppgift HTML = new Uppgift("U100", "HTML - Lab1", "Lång uppgiftsbeskrivning om HTML - Lab1 med sammor av text", null);
-        public Uppgift vatten = new Uppgift("U101", "Vattenfallsmetoden Lab1", "Lång uppgiftsbeskrivning om uppgiften med vattenvallsmetoden Lab1", null);
-
-        public List<Uppgift> uppgifter = new List<Uppgift>()
-        {
-
-        };
-
+        public List<Student> students { get; set; }
+        public List<Teacher> teachers { get; set; }
+        public List<Kurs> kurser { get; set; }
+        public List<Uppgift> uppgifter { get; set; }
 
 
         public Datastore()
         {
+            students = new List<Student>();
+            students.Add(new Student("S100", "Anders"));
+            students.Add(new Student("S101", "Börje"));
+            students.Add(new Student("S102", "Uffe"));
+            students.Add(new Student("S103", "Koffe"));
+            students.Add(new Student("S104", "Fredrik"));
+            students.Add(new Student("S105", "Johanes"));
+            students.Add(new Student("S106", "Louise"));
 
+            Teacher Roger = new Teacher("T100", "Roger");
+            Teacher Lisa = new Teacher("T101", "Lisa");
+
+            teachers = new List<Teacher>();
             teachers.Add(Roger);
             teachers.Add(Lisa);
+            teachers.Add(new Teacher("T102", "Erik"));
+            teachers.Add(new Teacher("T103", "Pelle"));
+            teachers.Add(new Teacher("T104", "Johan"));
+            teachers.Add(new Teacher("T104", "Johan"));
+            teachers.Add(new Teacher("T105", "Magnus"));
+            teachers.Add(new Teacher("T106", "Moa"));
+            teachers.Add(new Teacher("T107", "Lena"));
+
+
+            Kurs Sys = new Kurs("SysAdm", "System administation");
+            Kurs Web = new Kurs("WebDes", "WebbDesign");
+            kurser = new List<Kurs>();
+
+            Uppgift HTML = new Uppgift("U100", "HTML - Lab1", "Lång uppgiftsbeskrivning om HTML - Lab1 med sammor av text", null);
+            Uppgift vatten = new Uppgift("U101", "Vattenfallsmetoden Lab1", "Lång uppgiftsbeskrivning om uppgiften med vattenvallsmetoden Lab1", null);
+            uppgifter = new List<Uppgift>();
+
+
             Sys.AnsvLarare = Roger;
             Web.AnsvLarare = Lisa;
             vatten.kurs = Sys;
@@ -89,82 +76,10 @@ namespace LAB2___UnicornCommandBridge
             Web.Elever = students;
             Web.Larare = teachers;
             Web.KursUppgifter.Add(HTML);
-            
+
 
             kurser.Add(Sys);
             kurser.Add(Web);
         }
     }
-    
 }
-        //public List<Student> students = new List<Student>()
-        //{
-        //    new Student("S100", "Anders"),
-        //    new Student("S101", "Börje"),
-        //    new Student("S102", "Uffe"),
-        //    new Student("S103", "Koffe"),
-        //    new Student("S104", "Fredrik"),
-        //    new Student("S105", "Johanes"),
-        //    new Student("S106", "Louise")
-        //};
-
-        //List<Betyg> sb = new List<Betyg>();     
-
-        //public List<Teacher> teachers = new List<Teacher>()
-        //{
-        //    new Teacher("T100", "Roger"),
-        //    new Teacher("T101", "Lisa"),
-        //    new Teacher("T102", "Erik"),
-        //    new Teacher("T103", "Pelle"),
-        //    new Teacher("T104", "Johan"),
-        //    new Teacher("T105", "Magnus"),
-        //    new Teacher("T106", "Moa"),
-        //    new Teacher("T107", "Lena")
-        //};
-
-        //public List<Kurs> kurser = new List<Kurs>()
-        //{
-        //    new Kurs("SysAdm", "System administation"),
-        //    new Kurs("WebDes", "WebbDesign")
-        //}
-        //    ;
-
-        //private List<Betyg> ub = new List<Betyg>();
-
-        //public List<Uppgift> uppgifter = new List<Uppgift>()
-        //{
-        //    new Uppgift("U100", "HTML - Lab1", "Lång uppgiftsbeskrivning om HTML - Lab1 med sammor av text", null),
-        //    new Uppgift("U101", "Vattenfallsmetoden Lab1", "Lång uppgiftsbeskrivning om uppgiften med vattenvallsmetoden Lab1", null)
-        //};
-
-        //List<Teacher> teachA = new List<Teacher>();
-        //List<Teacher> teachB = new List<Teacher>();
-        //List<Student> studA = new List<Student>();
-        //List<Student> studB = new List<Student>();
-
-        //public Datastore()
-        //{
-        //    foreach (Student stud in students)
-        //    {
-        //        stud.betyg = sb;
-        //    }
-        //    foreach (Teacher t in teachers)
-        //    {
-        //        teachA.Add(t);
-        //    }
-        //    foreach (Teacher t in teachers)
-        //    {
-        //        teachB.Add(t);
-        //    }
-        //    foreach (Student s in students)
-        //    {
-        //        studA.Add(s);
-        //    }
-        //    foreach (Student s in students)
-        //    {
-        //        studB.Add(s);
-        //    }
-        //    foreach (Kurs k in kurser)
-        //    {
-        //        k.AnsvLarare = teachers[0];
-        //    }
---
+++ LAB2 - UnicornCommandBridge/IDatastore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LAB2___UnicornCommandBridge
+{
+    public interface IDatastore
+    {
+        List<Kurs> kurser { get; set; }
+        List<Student> students { get; set; }
+        List<Teacher> teachers { get; set; }
+        List<Uppgift> uppgifter { get; set; }
+    }
+}
